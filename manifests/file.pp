f66615bd2e71b1cdae31a3e9e2f68be0c11e6857
Adam Jahn <ajjahn@gmail.com>
2013-07-03 12:17:35 -0400

    setup admin bash profile

--- manifests/file.pp
+++ manifests/file.pp
@@ -9,4 +9,24 @@ class administration::file {
     require => Class["administration::directory"]
   }
   
+  file { "bash_profile":
+    path => "/home/${administration::username}/.bash_profile",
+    ensure => file,
+    owner => $administration::username,
+    group => $administration::username,
+    mode => 600,
+    source => "puppet:///modules/${module_name}/bash_profile",
+    require => Class["administration::directory"]
+  }
+  
+  file { "bashrc":
+    path => "/home/${administration::username}/.bashrc",
+    ensure => file,
+    owner => $administration::username,
+    group => $administration::username,
+    mode => 600,
+    source => "puppet:///modules/${module_name}/bashrc",
+    require => Class["administration::directory"]
+  }
+  
 }
